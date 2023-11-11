5af94fd8552f449d0624d27df81cf5c11b0efa9b
110211 <35395659+110211@users.noreply.github.com>
2023-11-12 02:12:58 +0700

    thêm chức năng đăng nhập (sẽ cải thiện thêm)

---
+++ app/src/main/java/com/example/apptruyentranh2/activity_login.java
@@ -0,0 +1,73 @@
+package com.example.apptruyentranh2;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class activity_login extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Khởi tạo FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Ánh xạ các thành phần UI
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        findViewById(R.id.buttonLogin).setOnClickListener(view -> loginUser());
+
+        // Button để chuyển đến RegisterActivity
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button buttonGoToRegister = findViewById(R.id.buttonGoToRegister);
+
+        buttonGoToRegister.setOnClickListener(view -> {
+            Intent intent = new Intent(activity_login.this, activity_register.class);
+            startActivity(intent);
+        });
+    }
+
+    private void loginUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        // Kiểm tra hợp lệ của email và password
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Vui lòng nhập đầy đủ email và mật khẩu", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Đăng nhập bằng Firebase Authentication
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Đăng nhập thành công, xử lý sau khi đăng nhập
+                        handleLoginSuccess();
+                    } else {
+                        // Đăng nhập thất bại, xử lý thông báo lỗi
+                        Toast.makeText(activity_login.this, "Đăng nhập thất bại: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void handleLoginSuccess() {
+        // Xử lý sau khi đăng nhập thành công, ví dụ: chuyển đến màn hình chính
+        Intent intent = new Intent(activity_login.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/apptruyentranh2/activity_register.java
@@ -0,0 +1,67 @@
+package com.example.apptruyentranh2;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.Objects;
+
+public class activity_register extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextEmail, editTextPassword;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        Button buttonRegister = findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(view -> registerUser());
+    }
+
+    private void registerUser() {
+        String username = editTextUsername.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        // Kiểm tra hợp lệ của thông tin đăng ký
+        if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Đăng ký bằng Firebase Authentication
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Đăng ký thành công, xử lý sau khi đăng ký
+                        Toast.makeText(activity_register
+                                .this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                        finish(); // Kết thúc màn hình đăng ký và quay lại màn hình đăng nhập hoặc màn hình chính
+                    } else {
+                        // Đăng ký thất bại, xử lý thông báo lỗi
+                        if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                            Toast.makeText(activity_register
+                                    .this, "Email đã được sử dụng", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(activity_register
+                                    .this, "Đăng ký thất bại: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
+
